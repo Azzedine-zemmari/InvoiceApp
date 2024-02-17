b51d1e4a0215bccafdb86141914d2c7eded203ff
Azzedine zemmari <98091150+Azzedine-zemmari@users.noreply.github.com>
2024-02-18 00:34:55 +0100

    add the model chunk

--- resources/js/components/Invoice/new.vue
+++ resources/js/components/Invoice/new.vue
@@ -4,6 +4,10 @@ import {onMounted,ref} from 'vue'
 let form = ref([])
 let allCustomers = ref([])
 let customer_id = ref([])
+let item = ref([])
+let listCart = ref([])
+let showModel = ref(false)
+let hiddeModel = ref(true)
 const indexForm = async()=>{
     let response = await axios.get('/api/createInvoice')
     // console.log('form',response.data)
@@ -18,6 +22,23 @@ const getAllCustomers = async() => {
     console.log('response',response)
     allCustomers.value = response.data.customers
 }
+const addCart = (item)=>{
+        const itemCart = {
+            id:item.id,
+            item_code:item.item_code,
+            Description:item.description,
+            unit_price:item.unit_price,
+            quantity:item.quantity,  
+        }
+        listCart.value.push(itemCart)
+}
+const openModel =()=>{
+showModel.value = !showModel.value
+}
+const closeModel = ()=>{
+    showModel   .value = !hiddeModel.value
+}
+
 </script>
 <template>
 <div class="container">
@@ -36,21 +57,24 @@ const getAllCustomers = async() => {
             <div class="card__content--header">
                 <div>
                     <p class="my-1">Customer</p>
-                    <select name="" id="" class="input">
-                        <option value="">cust 1</option>
+                    <select name="" id="" class="input" v-model="customer_id">
+                        <option value="" disabled>Select option</option>
+                        <option :value="customer.id" v-for="customer in allCustomers" :key="customer.id">
+                        {{ customer.firstname }}
+                        </option>
                     </select>
                 </div>
                 <div>
                     <p class="my-1">Date</p> 
-                    <input id="date" placeholder="dd-mm-yyyy" type="date" class="input"> <!---->
+                    <input id="date" placeholder="dd-mm-yyyy" type="date" class="input" v-model="form.date"> <!---->
                     <p class="my-1">Due Date</p> 
-                    <input id="due_date" type="date" class="input">
+                    <input id="due_date" type="date" class="input" v-model="form.due_date">
                 </div>
                 <div>
                     <p class="my-1">Numero</p> 
-                    <input type="text" class="input"> 
+                    <input type="text" class="input" v-model="form.number"> 
                     <p class="my-1">Reference(Optional)</p> 
-                    <input type="text" class="input">
+                    <input type="text" class="input" v-model="form.reference">
                 </div>
             </div>
             <br><br>
@@ -65,23 +89,24 @@ const getAllCustomers = async() => {
                 </div>
     
                 <!-- item 1 -->
-                <div class="table--items2">
-                    <p>#093654 vjxhchkvhxc vkxckvjkxc jkvjxckvjkx </p>
+                <div class="table--items2" v-for="(itemCart,i) in listCart" :key="itemCart.id">
+                    <p>#{{ itemCart.item_code }} {{ itemCart.Description }}</p>
                     <p>
-                        <input type="text" class="input" >
+                        <input type="text" class="input" v-model="itemCart.unit_price" >
                     </p>
                     <p>
-                        <input type="text" class="input" >
+                        <input type="text" class="input" v-model="itemCart.quantity">
                     </p>
-                    <p>
-                        $ 10000
+                    <p v-if="itemCart.quantity">
+                        $ {{ itemCart.quantity * itemCart.unit_price }}
                     </p>
+                    <p v-else></p>
                     <p style="color: red; font-size: 24px;cursor: pointer;">
                         &times;
                     </p>
                 </div>
                 <div style="padding: 10px 30px !important;">
-                    <button class="btn btn-sm btn__open--modal">Add New Line</button>
+                    <button class="btn btn-sm btn__open--modal" @click="openModel()">Add New Line</button>
                 </div>
             </div>
     
@@ -122,9 +147,9 @@ const getAllCustomers = async() => {
         
     </div>
     <!--==================== add modal items ====================-->
-    <div class="modal main__modal ">
+    <div class="modal main__modal "   :class="{show:showModel}">
         <div class="modal__content">
-            <span class="modal__close btn__close--modal">×</span>
+            <span class="modal__close btn__close--modal" @click="closeModel()">×</span>
             <h3 class="modal__title">Add Item</h3>
             <hr><br>
             <div class="modal__items">
